631fc078a7251c2bb281baacac699747ddeea98d
ant <antdwhitehead@gmail.com>
2020-06-03 19:37:52 +0100

    item show and index

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -4,23 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\Item;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ItemController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
-        //
+        return response(Item::all());
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
@@ -31,7 +32,7 @@ class ItemController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function store(Request $request)
     {
@@ -42,18 +43,18 @@ class ItemController extends Controller
      * Display the specified resource.
      *
      * @param  \App\Models\Item  $item
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function show(Item $item)
     {
-        //
+        return response($item);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  \App\Models\Item  $item
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function edit(Item $item)
     {
@@ -65,7 +66,7 @@ class ItemController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Item  $item
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function update(Request $request, Item $item)
     {
@@ -76,7 +77,7 @@ class ItemController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Item  $item
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function destroy(Item $item)
     {
