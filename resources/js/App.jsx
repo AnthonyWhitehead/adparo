b495a1b26e524596bc43dac271eb0c8f78ee832f
ant <antdwhitehead@gmail.com>
2020-06-11 08:58:39 +0100

    material ui login

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-const Component = () => <div> app</div>;
+import Login from './components/Login';
+
+const Component = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default Component;
